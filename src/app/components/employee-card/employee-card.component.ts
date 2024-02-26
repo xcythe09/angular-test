db1eab08fc713f656dda526f5dcb33292c272460
jonyjjo <jony_jjo@hotmail.com>
2024-02-26 09:42:31 +0000

    add employee card

---
+++ src/app/components/employee-card/employee-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-card',
+  templateUrl: './employee-card.component.html',
+  styleUrls: ['./employee-card.component.scss']
+})
+export class EmployeeCardComponent {
+
+}
